b680f1009da897ef9878933c8abd09840ea2b717
Achille <134864742+traliach@users.noreply.github.com>
2024-03-05 22:12:51 -0500

    Problem error message Solution First, remove 'https://' prefix.

--- terraform1/main.tf
+++ terraform1/main.tf
@@ -1,6 +1,12 @@
 locals {
-  // Remove 'https://' and trailing slashes from the storage account's primary web endpoint
-  storage_account_hostname = replace(replace(azurerm_storage_account.resume.primary_web_endpoint, "https://", ""), "/$", "")
+  // First, remove 'https://' prefix.
+  storage_account_hostname_temp = replace(azurerm_storage_account.resume.primary_web_endpoint, "https://", "")
+  
+  // Next, trim whitespace (though this might not be necessary, it's a precaution).
+  storage_account_hostname_trimmed = trimspace(local.storage_account_hostname_temp)
+  
+  // Finally, remove any trailing slash.
+  storage_account_hostname = replace(local.storage_account_hostname_trimmed, "/$", "")
 }
 
 
